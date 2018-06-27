64b5570645a1ed0b7d0bdc79791f66ec93f2811e
gorkem <gorkemkurt@gmail.com>
2018-06-27 21:30:23 +0100

    dev.js added

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - dev keys here!!
+module.exports = {
+  mongoURI: 'localhost:27017',
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN
+};
